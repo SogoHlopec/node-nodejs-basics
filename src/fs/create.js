71a3cd0c740f5aa4a159598b0728162bb4c3031d
SogoHlopec <sogo246@gmail.com>
2024-10-06 01:12:49 +0300

    feat: finished fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import * as fsPromises from 'node:fs/promises';
+import * as path from 'node:path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fsPromises.access(path.join(__dirname, '/files/fresh.txt'));
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+
+        try {
+            await fsPromises.writeFile(
+                path.join(__dirname, '/files/fresh.txt'),
+                'I am fresh and young'
+            );
+        } catch (writeError) {
+            throw new Error(writeError);
+        }
+    }
 };
 
 await create();
