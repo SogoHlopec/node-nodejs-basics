7479127b621ae54076990199f31b6a6e9e2fb6a8
SogoHlopec <sogo246@gmail.com>
2024-10-06 16:16:17 +0300

    feat: finished copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import * as fsPromises from 'node:fs/promises';
+import * as path from 'node:path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fsPromises.mkdir(path.join(__dirname, 'files_copy'));
+        const files = await fsPromises.readdir(path.join(__dirname, 'filess'));
+        for (const file of files) {
+            const existingFile = path.join(__dirname, 'files', file);
+            const fileCopy = path.join(__dirname, 'files_copy', file);
+            await fsPromises.copyFile(existingFile, fileCopy);
+        }
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
