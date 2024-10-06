0d032e7c4e42305c4c31372292bc8c6c9eb9d445
SogoHlopec <sogo246@gmail.com>
2024-10-06 18:49:35 +0300

    feat: finished read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import * as fsPromises from 'node:fs/promises';
+import * as path from 'node:path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await fsPromises.readFile(
+            path.join(__dirname, 'files/fileToRead.txt'),
+            'utf8'
+        );
+        console.log(content);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
