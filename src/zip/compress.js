926cba7b598f0d86b02cf6a0b9486ae75552e959
SogoHlopec <sogo246@gmail.com>
2024-10-06 23:51:35 +0300

    feat: finished compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import * as zlib from 'node:zlib';
+import * as stream from 'node:stream';
+import * as fs from 'node:fs';
+
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const gzip = await zlib.createGzip();
+        const file = await fs.createReadStream(
+            path.join(__dirname, '/files/fileToCompress.txt')
+        );
+        const compressedFile = await fs.createWriteStream(
+            path.join(__dirname, '/files/archive.gz')
+        );
+
+        stream.pipeline(file, gzip, compressedFile, (err) => {
+            if (err) {
+                console.error('Pipeline failed:', err);
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
 };
 
 await compress();
