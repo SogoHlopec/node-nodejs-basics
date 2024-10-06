b2dc32103927832a73c274420da77f9d8517d5fb
SogoHlopec <sogo246@gmail.com>
2024-10-06 23:38:34 +0300

    feat: finished streams transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import * as process from 'node:process';
+import * as stream from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        const reverseString = new stream.Transform({
+            transform(chunk, enc, callback) {
+                const string =
+                    chunk.toString().trim().split('').reverse().join('') + '\n';
+                this.push(string);
+                callback();
+            },
+        });
+        process.stdin.pipe(reverseString).pipe(process.stdout);
+    } catch (error) {
+        throw error;
+    }
 };
 
 await transform();
