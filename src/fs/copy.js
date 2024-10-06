172193cc386d4d60a69f620504b1feb5761fa1fe
SogoHlopec <sogo246@gmail.com>
2024-10-06 20:51:38 +0300

    refactor: change import node url

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,6 +1,6 @@
 import * as fsPromises from 'node:fs/promises';
 import * as path from 'node:path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
