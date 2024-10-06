59a3944d5ae3f6c038649b752c112475cd830f6c
SogoHlopec <sogo246@gmail.com>
2024-10-06 20:07:05 +0300

    feat: finished env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+import * as process from 'node:process';
+
+
 const parseEnv = () => {
-    // Write your code here 
+    try {
+        const keys = Object.keys(process.env);
+        keys.forEach(item => {
+            if (item.startsWith('RSS_')) {
+                console.log(`${item}=${process.env[item]}`);
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
 };
 
 parseEnv();
