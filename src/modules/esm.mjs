3fe1e16db0bf0116f33cae06100a607d4da05bc2
SogoHlopec <sogo246@gmail.com>
2024-10-06 21:12:19 +0300

    feat: finished esm

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { release, version } from 'node:os';
+import { createRequire } from 'node:module';
+import { createServer as createServerHttp } from 'node:http';
+import './files/c.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
 const random = Math.random();
-
 let unknownObject;
 
 if (random > 0.5) {
     unknownObject = require('./files/a.json');
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = require('./files/a.json');
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +37,4 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export { unknownObject, myServer };
