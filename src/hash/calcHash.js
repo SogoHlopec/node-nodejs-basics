760ea36e6a5f5c3edd4cb59d586fd18308a7f775
SogoHlopec <sogo246@gmail.com>
2024-10-06 21:37:17 +0300

    feat: finished calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import * as fsPromises from 'node:fs/promises';
+import * as path from 'node:path';
+import * as crypto from 'node:crypto';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const hash = await crypto.createHash('sha256');
+
+        const filehandle = await fsPromises.open(
+            path.join(__dirname, '/files/fileToCalculateHashFor.txt')
+        );
+        const stream = await filehandle.createReadStream();
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+        stream.on('end', () => {
+            console.log(`${hash.digest('hex')}`);
+        });
+    } catch (error) {
+        throw error;
+    }
 };
 
 await calculateHash();
