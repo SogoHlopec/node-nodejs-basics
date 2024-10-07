b0646f1dcc73fb91651ec27cc6c7170200ee9fe1
SogoHlopec <sogo246@gmail.com>
2024-10-07 20:36:12 +0300

    feat: finished cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import * as child_process from 'node:child_process';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    child_process.spawn(
+        'node',
+        [path.join(__dirname, 'files/script.js'), ...args],
+        {
+            stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+        }
+    );
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([
+    'someArgument1',
+    'someArgument2',
+    'someArgument3',
+    'someArgument4',
+]);
